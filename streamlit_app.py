138d5f835e6112966298f12089fc9286ac6993f0
MKoch-git <68339493+MKoch-git@users.noreply.github.com>
2023-08-24 16:42:10 +0200

    Add pick list for fruits to add

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,3 +38,8 @@ my_cur.execute("SELECT * from fruit_load_list")
 my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
+
+streamlit.header("What fruit would you like to add:")
+add_my_fruit = streamlit.multiselect("Add fruits:", list(my_data_rows.index),['banana'])
+show_added_fruit = my_data_rows.loc[add_my_fruit]
+streamlit.dataframe(show_added_fruit)
